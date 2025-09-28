89f8cf18e8c895daebcbfe657a340c802423ad6b
Sebastian <sebastian_tunjuelo82232@elpoli.edu.co>
2025-09-27 21:08:33 -0500

    Delete Taller_2-main/demo directory

--- Taller_2-main/demo/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO locals (id, code, floor, name) VALUES (1, 'A101', '1', 'Zapatería');
-INSERT INTO locals (id, code, floor, name) VALUES (2, 'B202', '2', 'Librería');
-INSERT INTO locals (id, code, floor, name) VALUES (3, 'C303', '3', 'Juguetería');
